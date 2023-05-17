7ac3b52d455c3494b749236516903dd3e5685db6
RehaBugrahanOzel <bugrahanozel1996@gmail.com>
2023-05-18 00:04:36 +0300

    Deploy

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router/router";
-import VueVideoPlayer from "@videojs-player/vue";
-import "video.js/dist/video-js.css";
-const app = createApp(App);
-app.use(router).use(VueVideoPlayer).mount("#app");
